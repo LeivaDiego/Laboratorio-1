81da2adcbb8832b8e1e78b3b50a30142d94370b0
LeivaDiego <lei21752@uvg.edu.gt>
2023-07-12 17:30:07 -0600

    V3.1
    Creacion del Test para la clase de kotlin, y correccion de la clase ItemData

--- Lab1/src/main/kotlin/ItemData.kt
+++ Lab1/src/main/kotlin/ItemData.kt
@@ -1,31 +1,31 @@
-class ItemData (val originalValue: Any){
-    fun getType(): String {
+class ItemData(val originalValue: Any?){
+    fun getType(): String? {
         when(originalValue){
             is String -> return "cadena"
             is Int -> return "entero"
             is Boolean -> return "booleano"
-            else -> return null!!
+            else -> return null
         }
 
     }
 
-    fun getInfo(): String {
+    fun getInfo(): String? {
         when(originalValue){
-            is String -> return "L ${originalValue.length}";
+            is String -> return "L${originalValue.length}";
             is Int ->{
                 val value = originalValue as Int
                 when{
                     (value % 10 == 0) -> return "M10"
                     (value % 5 == 0) -> return "M5"
                     (value % 2 == 0) -> return "M2"
-                    else -> return null!!
+                    else -> return null
                 }
             }
             is Boolean -> {
                 if (originalValue) {
                     return "Verdadero"
                 } else return "Falso"
-            } else -> return null!!
+            } else -> return null
         }
     }
 }
