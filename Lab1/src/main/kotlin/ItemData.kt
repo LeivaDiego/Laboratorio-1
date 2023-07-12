ab11b09915f1717a3f434a8b8b0475c5bbeaaae0
LeivaDiego <lei21752@uvg.edu.gt>
2023-07-12 17:41:36 -0600

    V4.0
    Documentacion de las clases

--- Lab1/src/main/kotlin/ItemData.kt
+++ Lab1/src/main/kotlin/ItemData.kt
@@ -1,4 +1,15 @@
+/**
+ * Clase de kotlin que muestra en consola mensajes especificos que le indican al usuario que tipo de dato tiene la
+ * variable originalValue e informacion extra dependiendo de que tipo de dato sea
+ * @author diego leiva
+ */
 class ItemData(val originalValue: Any?){
+
+    /**
+     * Funcion que muestra que tipo de dato tiene la variable originalValue
+     * @return  L"tamano cadena" si es string. M10, M5, M2, null si es integer y es multiplo de 10, 5, 2 o no aplica
+     * niguno de los anteriores respectivamente.Verdadero o Falso si esl Boolean. Y null si es otro tipo de dato
+     */
     fun getType(): String? {
         when(originalValue){
             is String -> return "cadena"
@@ -9,6 +20,10 @@ class ItemData(val originalValue: Any?){
 
     }
 
+    /**
+     * Funcion que muestra informacion del valor dependiendo de que tipo de dato sea
+     * @return un string con el tipo de dato, ya sea String, Integer, Boolean o null si no es niguno de los anteriores
+     */
     fun getInfo(): String? {
         when(originalValue){
             is String -> return "L${originalValue.length}";
--- Lab1/src/main/kotlin/ItemDataJ.java
+++ Lab1/src/main/kotlin/ItemDataJ.java
@@ -1,45 +1,56 @@
+/**
+ * Clase de java que muestra en consola mensajes especificos que le indican al usuario que tipo de dato tiene la
+ * variable originalValue e informacion extra dependiendo de que tipo de dato sea
+ * @author diego leiva
+ */
 public class ItemDataJ {
-    Object originalValue;
+    private Object originalValue;
     public ItemDataJ(Object originalValue){
         this.originalValue = originalValue;
     }
 
+    /**
+     * Metodo que muestra que tipo de dato tiene la variable originalValue
+     * @return un string con el tipo de dato, ya sea String, Integer, Boolean o null si no es niguno de los anteriores
+     */
     public String getType(){
         if (originalValue instanceof String){
             return "cadena";
-        }
-        else if (originalValue instanceof Integer){
+        } else if (originalValue instanceof Integer){
             return "entero";
-        }
-        else if (originalValue instanceof Boolean){
+        } else if (originalValue instanceof Boolean){
             return "booleano";
+        } else {
+            return null;
         }
-        else return null;
     }
 
+    /**
+     * Metodo que muestra informacion del valor dependiendo de que tipo de dato sea
+     * @return L"tamano cadena" si es string. M10, M5, M2, null si es integer y es multiplo de 10, 5, 2 o no aplica
+     * niguno de los anteriores respectivamente.Verdadero o Falso si esl Boolean. Y null si es otro tipo de dato
+     */
     public String getInfo(){
         if (originalValue instanceof String) {
             return "L" + ((String) originalValue).length();
-        }
-        else if (originalValue instanceof Integer){
-            if ((Integer)originalValue % 10 == 0){
+        } else if (originalValue instanceof Integer value){
+            if (value % 10 == 0){
                 return "M10";
-            }
-            else if ((Integer)originalValue % 5 == 0){
+            } else if (value % 5 == 0){
                 return "M5";
-            }
-            else if ((Integer)originalValue % 2 == 0){
+            } else if (value % 2 == 0){
                 return "M2";
+            } else {
+                return null;
             }
-            else return null;
-        }
-        else if (originalValue instanceof Boolean){
+        } else if (originalValue instanceof Boolean){
             if ((Boolean)originalValue){
                 return "Verdadero";
-            }else return "Falso";
+            }else {
+                return "Falso";
+            }
+        } else {
+            return null;
         }
-        else return null;
     }
-
-
 }
