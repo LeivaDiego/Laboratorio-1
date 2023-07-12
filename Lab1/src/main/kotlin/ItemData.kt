c134f7ec7239668b7cdf26731c876596f0128cb7
LeivaDiego <lei21752@uvg.edu.gt>
2023-07-12 17:20:17 -0600

    V3.0
    Clase de kotlin implementada

--- Lab1/src/main/kotlin/ItemData.kt
+++ Lab1/src/main/kotlin/ItemData.kt
@@ -1,9 +1,31 @@
 class ItemData (val originalValue: Any){
     fun getType(): String {
-        
+        when(originalValue){
+            is String -> return "cadena"
+            is Int -> return "entero"
+            is Boolean -> return "booleano"
+            else -> return null!!
+        }
+
     }
 
-    fun getInfo(): Int {
-        return 0
+    fun getInfo(): String {
+        when(originalValue){
+            is String -> return "L ${originalValue.length}";
+            is Int ->{
+                val value = originalValue as Int
+                when{
+                    (value % 10 == 0) -> return "M10"
+                    (value % 5 == 0) -> return "M5"
+                    (value % 2 == 0) -> return "M2"
+                    else -> return null!!
+                }
+            }
+            is Boolean -> {
+                if (originalValue) {
+                    return "Verdadero"
+                } else return "Falso"
+            } else -> return null!!
+        }
     }
 }
